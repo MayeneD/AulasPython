8f86b7f0a1c0912897f04089c9704c413dd573cc
eneyam <maygdoria@gmail.com>
2024-03-06 20:52:15 -0300

    Segunda aula pdf02

---
+++ aula02.py
@@ -0,0 +1,30 @@
+# numero = float(round(3.3224, 2))
+# print('O número com duas casas decimais é',numero,'.')
+# print('O número com duas casas decimais é '+str(numero)+'.')
+# print('O número com duas casas decimais é',str(numero)+'.')
+#
+#
+# numero1 = 3.3224
+# print("O número com duas casas decimais é %.2f." %numero1)
+#
+#
+# numero2 = 3.3224
+# print('O número com duas casas decimais é {:.4f}.'.format(numero2))
+#
+# numero3 = 3.3224
+# print(f"O número com duas casas decimais é {numero2:.3f}.")
+#
+# pi =3.1415
+# print(f"O numero pi com duas casas decimais é {pi:.2f}")
+#
+#
+# d = True
+# e = False
+# print(type(d))
+#
+#
+
+# nota = 6
+# media = 7
+# aprovado = nota >= media
+# print(aprovado)
